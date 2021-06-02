56e3bdf68d8c3d9607ec9c1d1f424a9be4ca1047
Hellogark <edmolo_0@hotmail.com>
2021-06-02 14:14:25 -0600

    Se creó el navbar responsivo. Se añadió animación al navbar responsivo.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect} from 'react'
+import React, { useState,useEffect} from 'react'
 import './Navbar.css';
 import { useTranslation } from 'react-i18next'
 import  { Link } from "react-scroll";
@@ -7,19 +7,23 @@ import { changeClass } from '../../redux/colorDuck';
 const Navbar = () => {
     const dispatch = useDispatch();
     const {navColors} = useSelector(store => store);
-console.log(navColors);
     const { t, i18n } = useTranslation();
+    const [responsiveNavState, setResponsiveNavState] = useState(false);
 
 
     useEffect(() => {
         const prev = navColors.state && navColors.state.bgClass  ? navColors.state.bgClass : null;
         const next = navColors.bgClass;
         let nav = document.getElementById("nav");
+        let navList = document.getElementById("listaNav");
         if(!prev){
             nav.classList.add(next);
+            navList.classList.add(next);
         } else {
             nav.classList.remove(prev);
             nav.classList.add(next);
+            navList.classList.remove(prev);
+            navList.classList.add(next);
         }
     }, [navColors])
 
@@ -29,11 +33,42 @@ console.log(navColors);
     const selectLanguageHandler = (language) => {
             i18n.changeLanguage(language);
     } 
+    const scrollSection = ( bgClass, dispatch) => {
+        dispatch(changeClass(bgClass))
+    }
+    
+    const changeNavbarState = () => {
+        let ham = document.getElementById("ham");
+        ham.style.pointerEvents = "none";
+        let navList = document.getElementById("listaNav");
+        if(!responsiveNavState) {
+            ham.classList.add("ham-active");
+            navList.classList.add("openNav");
+            document.documentElement.style.setProperty('--display-nav', 'flex');
+            setResponsiveNavState(true);
+            ham.style.pointerEvents = "auto";
+        } else {
+            ham.classList.toggle("ham-active");
+            navList.classList.add("closedNav");
+            navList.classList.remove("openNav");
+            ham.classList.add("ham-inactive");
+            ham.classList.remove("ham-active");
+            setTimeout(() => {
+                document.documentElement.style.setProperty('--display-nav', 'none');
+                ham.classList.remove("ham-inactive");
+                ham.classList.remove("ham-active");
+                setResponsiveNavState(false);
+                ham.style.pointerEvents = "auto";
+            }, 700);
+
+
+        }
+    }
 
     return (
         <header className="header">
             <nav className="nav" id="nav">
-                <ul className="listaNav">
+                <ul className="listaNav" id="listaNav">
                     <li className="listItem" >
                         <Link
                             onClick={() =>  scrollSection( 'bg__nav__home', dispatch )}
@@ -78,13 +113,16 @@ console.log(navColors);
                     </li>
                     <div className="listItem"><span onClick={() => selectLanguageHandler('en') }>EN</span><span style={{margin: '0 10px'}}>|</span><span  onClick={ () => selectLanguageHandler('es') } >ES</span></div>
                 </ul>
+                <div className="ham-container" id="ham" onClick={changeNavbarState}>
+                    <span className="ham-line"></span>
+                    <span className="ham-line"></span>
+                    <span className="ham-line"></span>
+                </div>
             </nav>
         </header>
     )
+
 }
 
-const scrollSection = ( bgClass, dispatch) => {
-    dispatch(changeClass(bgClass))
-}
 
 export default Navbar
