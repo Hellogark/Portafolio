22c155e4675e0a00286351a8dcba595db7ee8cd1
Hellogark <edmolo_0@hotmail.com>
2021-06-16 01:00:03 -0600

    Second fix

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -9,10 +9,8 @@ import ScrollWatcher from 'scroll-watcher'
 const Contact = () => {
 const { t, i18n } = useTranslation();    
 const dispatch = useDispatch();
-const scroll = new ScrollWatcher();
-const scrollSection = ( bgClass, dispatch) => {
-    dispatch(changeClass(bgClass));
-
+const scrollSection = ( bgClass, dispatcher) => {
+    dispatcher(changeClass(bgClass));
 }
 const downloadPdf = () =>{
     i18n.language === 'es' ? 
@@ -21,6 +19,7 @@ const downloadPdf = () =>{
     saveAs("https://res.cloudinary.com/eduardocloud/image/upload/v1623808791/Media/Eduardo-Moreno-CV-EN.pdf", "Eduardo-Moreno-CV");
 }
 useEffect(() => {
+    const scroll = new ScrollWatcher();
     window.innerWidth <= 768 ?
     scroll
   .watch(".contact-section")
